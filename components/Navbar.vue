3dd2af8d6f27f35f85380d4ba3f01bff554e789c
miluge <gllmblndl@gmail.com>
2021-07-13 14:27:13 +0200

    Remove NuxtLogo and Tutorial components Create Layouts dir and default.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,9 @@
 <template>
-
+  <nav class="bg-black fixed h-16 w-full">
+    <ul class="text-black">
+      <li>A</li>
+      <li>B</li>
+      <li>C</li>
+    </ul>
+  </nav>
 </template>
-
-<script>
-export default {
-  name: "Navbar"
-}
-</script>
-
-<style scoped>
-
-</style>
