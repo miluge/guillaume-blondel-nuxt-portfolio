a7dff3327f29aaadc53bec56394ddd73fd1bf8bb
miluge <gllmblndl@gmail.com>
2021-08-17 14:40:17 +0200

    Add nuxt-gsap-module to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss', 'nuxt-gsap-module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -42,5 +42,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  gsap: {
+    /* module options */
   }
 }
